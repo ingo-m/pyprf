251f4169516f0dbd7df5623fb37aa39eb9f373fd
Ingo Marquardt <ingo.marquardt@gmx.de>
2017-10-13 18:26:32 +0200

    Config information from CSV file. Work in progerss.

---
+++ py_pRF_mapping/analysis/__main__.py
@@ -0,0 +1,58 @@
+"""
+Entry point.
+
+References
+----------
+https://chriswarrick.com/blog/2014/09/15/python-apps-the-right-way-entry_points-and-scripts/
+
+Notes
+-----
+Use config.py to set analysis parameters.
+"""
+
+import argparse
+
+
+# def main(lstArgs=None):
+def main():
+    """py_pRF_mapping entry point."""
+    # Get list of input arguments (without first one, which is the path to the
+    # function that is called):  --NOTE: This is another way of accessing
+    # input arguments, but since we use 'argparse' it is redundant.
+    # lstArgs = sys.argv[1:]
+
+    # Create parser object:
+    objParser = argparse.ArgumentParser()
+
+    # Add argument to namespace:
+    objParser.add_argument('-yeah', metavar='yolo', default='tutu',
+                           help='Have a nice day.')
+
+    # Namespace object containign arguments and values:
+    objNspc = objParser.parse_args()
+
+    print('type(objNspc)')
+    print(type(objNspc))
+
+    print('objNspc')
+    print(objNspc)
+
+    # objParser.add_argument(
+    #     'config', metavar='analysis/config.py',
+    #     help=('Path to config file (if not using config file in standard \
+    #            location, i.e. ~/analysis/config.py.')
+    #            )
+
+    # print(lstArgs)
+
+    # lstArgs = objParser.parse_lstArgs()
+
+    # print(lstArgs)
+
+#...
+
+    # Close file:
+    # fleConfig.close()
+
+if __name__ == "__main__":
+    main()
--- setup.py
+++ setup.py
@@ -10,19 +10,20 @@ from setuptools import setup
 # except ImportError:
 #     from distutils.core import setup
 
+with open('README.md') as f:
+    long_description = f.read()
+
 setup(name='py_pRF_mapping',
       version='1.1.0',
       description=('A free & open source python tool for population receptive \
                     field analysis of fMRI data.'),
       url='https://github.com/ingo-m/py_pRF_mapping',
-      download_url='',
       author='Ingo Marquardt',
-      author_email='ingo.marquardt@gmx.de',
       license='GNU General Public License Version 3',
       packages=['analysis'],
       install_requires=['numpy', 'scipy', 'nibabel'],
       keywords=['pRF', 'fMRI', 'retinotopy'],
-      zip_safe=True,
+      long_description=long_description,
       entry_points={
           'console_scripts': [
               'py_pRF_mapping = analysis.__main__:main',
