b6928af8add72505e1c95b7fb466032f27654b01
Ingo Marquardt <ingo.marquardt@gmx.de>
2017-10-22 21:26:55 +0200

    Cython compilation included in test.

--- pyprf/analysis/testing/test_utils.py
+++ pyprf/analysis/testing/test_utils.py
@@ -5,6 +5,10 @@ from os.path import isfile, join
 import numpy as np
 from .. import pyprf_main
 from .. import utilities
+from cython_leastsquares_setup_call import setup_cython
+
+
+setup_cython()
 
 # Get directory of this file:
 strDir = os.path.dirname(os.path.abspath(__file__))
