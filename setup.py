b058087f9bad7eb451edb1fe77d4417a0ecce635
Ingo Marquardt <ingo.marquardt@gmx.de>
2018-01-04 22:33:06 +0100

    Compile cython with numpy dependency. It does not seem to be possible to compile python without the numpy dependency, so the user will have to install numpy beforehands. All other dependencies (including cython) should be handled by the installer (even if using pip install).

--- setup.py
+++ setup.py
@@ -5,39 +5,9 @@ For development installation:
     pip install -e /path/to/pRF_mapping
 """
 
-from setuptools import setup  #, Extension
+import numpy as np
+from setuptools import setup, Extension
 # from setuptools.command.build_ext import build_ext
-
-
-# class clssBldNmp(build_ext):
-#     """
-#     Class for handling numpy dependency for cython setup.
-#
-#     Subclass of `setuptools.command.build_py.build_py`
-#
-#     In order to install the cython extensions, the
-#     `ext_modules=[Extension(...)]` argument of the `setuptools.setup(...)`
-#     function is needed. Problematically, the expression `numpy.get_include()`
-#     needs to be passed into this function. Thus, there is a `numpy` dependency
-#     in this `setup.py` file. Thus, `import numpy` needs to be executed before
-#     reaching the `setuptools.setup(...)` command, which specifies the numpy
-#     dependency - a chicken-and-egg problem. With this class, we add a custom
-#     build command to the `setuptools.setup(...)` command. This is somewhat of a
-#     workaround, but it seems necessary in order to make pyprf installable
-#     through pip with full cython functionality.
-#
-#     This solution is based on the following references:
-#     https://stackoverflow.com/a/21621689
-#     https://seasonofcode.com/posts/how-to-add-custom-build-steps-and-commands-to-setuppy.html
-#     """
-#
-#     def finalize_options(self):
-#         """Workaround for numpy dependency of cython setup."""
-#         build_ext.finalize_options(self)
-#         # Prevent numpy from thinking it is still in its setup process:
-#         __builtins__.__NUMPY_SETUP__ = False
-#         import numpy
-#         self.include_dirs.append(numpy.get_include())
 
 with open('README.md') as f:
     long_description = f.read()
@@ -60,11 +30,10 @@ setup(name='pyprf',
           'console_scripts': [
               'pyprf = pyprf.analysis.__main__:main',
               ]},
-      # cmdclass={'build_ext': clssBldNmp},
-      # ext_modules=[Extension('pyprf.analysis.cython_leastsquares',
-      #                     ['pyprf/analysis/cython_leastsquares.pyx'],
-      #                     include_dirs=[numpy.get_include()]
-      #                     )],
+      ext_modules=[Extension('pyprf.analysis.cython_leastsquares',
+                             ['pyprf/analysis/cython_leastsquares.pyx'],
+                             include_dirs=[np.get_include()]
+                             )],
       )
 
 # Load module to setup python:
