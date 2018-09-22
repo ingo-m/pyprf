fd52717649b472d2689ebc5c2dc0e840809ea66d
Ingo Marquardt <ingo.marquardt@gmx.de>
2018-09-22 19:15:01 +0200

    Docstring changes - mentioning of new return argument (parameter estimates).

--- pyprf/analysis/cython_leastsquares.pyx
+++ pyprf/analysis/cython_leastsquares.pyx
@@ -4,18 +4,18 @@
 # Part of py_pRF_mapping library
 # Copyright (C) 2016  Omer Faruk Gulban & Ingo Marquardt
 #
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
+# This program is free software: you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free Software
+# Foundation, either version 3 of the License, or (at your option) any later
+# version.
 #
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+# details.
 #
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+# You should have received a copy of the GNU General Public License along with
+# this program.  If not, see <http://www.gnu.org/licenses/>.
 
 
 # *****************************************************************************
@@ -54,7 +54,10 @@ cpdef tuple cy_lst_sq(np.ndarray[np.float32_t, ndim=1] vecPrfTc,
     -------
     vecRes : np.array
         1D numpy array with model residuals for all voxels in the chunk of
-        functional data. Dimensionality: vecRes[voxel]
+        functional data. Dimensionality: vecRes[voxels].
+    vecPe : np.array
+        1D numpy array with parameter estimates ('betas') for all voxels in the
+        chunk of functional data. Dimensionality: vecRes[voxels].
 
     Notes
     -----
