7f6f96b44bb7c93c4d9166cf1a65ed535d5b9f7e
Ingo Marquardt <ingo.marquardt@gmx.de>
2018-09-25 22:50:13 +0200

    Cython setup tests.

--- pyprf/analysis/model_creation_timecourses_par.py
+++ pyprf/analysis/model_creation_timecourses_par.py
@@ -18,8 +18,8 @@
 # this program.  If not, see <http://www.gnu.org/licenses/>.
 
 import numpy as np
-#from pyprf.analysis.cython_leastsquares import cy_lst_sq
-#from pyprf.analysis.cython_leastsquares_two import cy_lst_sq_two
+from pyprf.analysis.cython_leastsquares import cy_lst_sq
+from pyprf.analysis.cython_leastsquares_two import cy_lst_sq_two
 #from pyprf.analysis.cython_prf_convolve import prf_conv
 import scipy as sp
 from pyprf.analysis.utilities import crt_gauss
