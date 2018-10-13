747b08ef00c52a57b848056821d30de2f2b698b3
Ingo Marquardt <ingo.marquardt@gmx.de>
2018-10-13 19:11:32 +0200

    Functions for linear trend removal & temporal smoothing moved out of parent function (so that they can be called from other modules). Removed redundant (commented out) legacy code.

--- pyprf/analysis/preprocessing_par.py
+++ pyprf/analysis/preprocessing_par.py
@@ -24,6 +24,114 @@ from scipy.ndimage.filters import gaussian_filter
 from scipy.ndimage.filters import gaussian_filter1d
 
 
+# *****************************************************************************
+# *** Linear trend removal for fMRI data
+
+def funcLnTrRm(idxPrc, aryFuncChnk, varSdSmthSpt, queOut):
+    """
+    Perform linear trend removal on the input fMRI data.
+
+    The variable varSdSmthSpt is not needed, only included for consistency
+    with other functions using the same parallelisation.
+    """
+    # Number of voxels in this chunk:
+    # varNumVoxChnk = aryFuncChnk.shape[0]
+
+    # Number of time points in this chunk:
+    varNumVol = aryFuncChnk.shape[1]
+
+    # We reshape the voxel time courses, so that time goes down the column,
+    # i.e. from top to bottom.
+    aryFuncChnk = aryFuncChnk.T
+
+    # Linear mode to fit to the voxel time courses:
+    vecMdlTc = np.linspace(0,
+                           1,
+                           num=varNumVol,
+                           endpoint=True,
+                           dtype=np.float32)
+    # vecMdlTc = vecMdlTc.flatten()
+
+    # We create a design matrix including the linear trend and a
+    # constant term:
+    aryDsgn = np.vstack([vecMdlTc,
+                         np.ones(len(vecMdlTc), dtype=np.float32)]).T
+    aryDsgn = aryDsgn.astype(np.float32, copy=False)
+
+    # Calculate the least-squares solution for all voxels:
+    aryLstSqFt = np.linalg.lstsq(aryDsgn, aryFuncChnk, rcond=None)[0]
+
+    # Multiply the linear term with the respective parameters to obtain the
+    # fitted line for all voxels:
+    aryLneFt = np.multiply(vecMdlTc[:, None],
+                           aryLstSqFt[0, :],
+                           dtype=np.float32)
+
+    # Using the least-square fitted model parameters, we remove the linear
+    # term from the data:
+    aryFuncChnk = np.subtract(aryFuncChnk,
+                              aryLneFt,
+                              dtype=np.float32)
+
+    # Using the constant term, we remove the mean from the data:
+    # aryFuncChnk = np.subtract(aryFuncChnk,
+    #                           aryLstSqFt[1, :])
+
+    # Bring array into original order (time from left to right):
+    aryFuncChnk = aryFuncChnk.T
+
+    # Output list:
+    lstOut = [idxPrc,
+              aryFuncChnk.astype(np.float32, copy=False)]
+
+    queOut.put(lstOut)
+# *************************************************************************
+
+
+# *************************************************************************
+# *** Temporal smoothing of fMRI data & pRF time course models
+
+def funcSmthTmp(idxPrc, aryFuncChnk, varSdSmthTmp, queOut):
+    """
+    Apply temporal smoothing to the input data.
+
+    The extend of smoothing needs to be specified as an input parameter.
+    """
+    # For the filtering to perform well at the ends of the time series, we
+    # set the method to 'nearest' and place a volume with mean intensity
+    # (over time) at the beginning and at the end.
+    aryFuncChnkMean = np.mean(aryFuncChnk,
+                              axis=1,
+                              keepdims=True,
+                              dtype=np.float32)
+
+    aryFuncChnk = np.concatenate((aryFuncChnkMean,
+                                  aryFuncChnk,
+                                  aryFuncChnkMean), axis=1)
+
+    # In the input data, time goes from left to right. Therefore, we apply
+    # the filter along axis=1.
+    aryFuncChnk = gaussian_filter1d(aryFuncChnk,
+                                    varSdSmthTmp,
+                                    axis=1,
+                                    order=0,
+                                    mode='nearest',
+                                    truncate=4.0)
+
+    # Remove mean-intensity volumes at the beginning and at the end:
+    aryFuncChnk = aryFuncChnk[:, 1:-1]
+
+    # Output list:
+    lstOut = [idxPrc,
+              aryFuncChnk.astype(np.float32, copy=False)]
+
+    queOut.put(lstOut)
+# *****************************************************************************
+
+
+# *****************************************************************************
+# *** Parent function for preprocessing of fMRI data and pRF time courses.
+
 def pre_pro_par(aryFunc, aryMask=np.array([], dtype=np.int16),  #noqa
                 lgcLinTrnd=False, varSdSmthTmp=0.0, varSdSmthSpt=0.0,
                 varPar=1):
@@ -328,146 +436,6 @@ def pre_pro_par(aryFunc, aryMask=np.array([], dtype=np.int16),  #noqa
 
     #     # And... done.
     #     return aryRes
-    # *************************************************************************
-
-    # *************************************************************************
-    # *** Linear trend removal for fMRI data
-
-    def funcLnTrRm(idxPrc, aryFuncChnk, varSdSmthSpt, queOut):
-        """
-        Perform linear trend removal on the input fMRI data.
-
-        The variable varSdSmthSpt is not needed, only included for consistency
-        with other functions using the same parallelisation.
-        """
-        # Number of voxels in this chunk:
-        # varNumVoxChnk = aryFuncChnk.shape[0]
-
-        # Number of time points in this chunk:
-        varNumVol = aryFuncChnk.shape[1]
-
-        # We reshape the voxel time courses, so that time goes down the column,
-        # i.e. from top to bottom.
-        aryFuncChnk = aryFuncChnk.T
-
-        # Linear mode to fit to the voxel time courses:
-        vecMdlTc = np.linspace(0,
-                               1,
-                               num=varNumVol,
-                               endpoint=True,
-                               dtype=np.float32)
-        # vecMdlTc = vecMdlTc.flatten()
-
-        # We create a design matrix including the linear trend and a
-        # constant term:
-        aryDsgn = np.vstack([vecMdlTc,
-                             np.ones(len(vecMdlTc), dtype=np.float32)]).T
-        aryDsgn = aryDsgn.astype(np.float32, copy=False)
-
-        # Calculate the least-squares solution for all voxels:
-        aryLstSqFt = np.linalg.lstsq(aryDsgn, aryFuncChnk, rcond=None)[0]
-
-        # Multiply the linear term with the respective parameters to obtain the
-        # fitted line for all voxels:
-        aryLneFt = np.multiply(vecMdlTc[:, None],
-                               aryLstSqFt[0, :],
-                               dtype=np.float32)
-
-        # Using the least-square fitted model parameters, we remove the linear
-        # term from the data:
-        aryFuncChnk = np.subtract(aryFuncChnk,
-                                  aryLneFt,
-                                  dtype=np.float32)
-
-        # Using the constant term, we remove the mean from the data:
-        # aryFuncChnk = np.subtract(aryFuncChnk,
-        #                           aryLstSqFt[1, :])
-
-        # Bring array into original order (time from left to right):
-        aryFuncChnk = aryFuncChnk.T
-
-        # Output list:
-        lstOut = [idxPrc,
-                  aryFuncChnk.astype(np.float32, copy=False)]
-
-        queOut.put(lstOut)
-    # *************************************************************************
-
-    # *************************************************************************
-    # ***  Spatial smoothing of fMRI data
-
-    # NOTE: This function is not used; because of memory limitations the
-    # spatial smoothing should not be parallelised over volumes. The spatial
-    # smoothing is performed below without a parallelisation wrapper, using a
-    # direct call to the respective numpy function.
-
-    # def funcSmthSpt(idxPrc, aryFuncChnk, varSdSmthSpt, queOut):
-    #     """
-    #     Apply spatial smoothing to the input data.
-
-    #     The extent of smoothing needs to be specified as an input parameter.
-    #     """
-    #     # Number of time points in this chunk:
-    #     varNumVol = aryFuncChnk.shape[3]
-
-    #     # Input data should already be float32. Just to be sure:
-    #     aryFuncChnk = aryFuncChnk.astype(np.float32, copy=False)
-
-    #     # Loop through volumes:
-    #     for idxVol in range(0, varNumVol):
-
-    #         aryFuncChnk[:, :, :, idxVol] = gaussian_filter(
-    #             aryFuncChnk[:, :, :, idxVol],
-    #             varSdSmthSpt,
-    #             order=0,
-    #             mode='nearest',
-    #             truncate=4.0).astype(np.float32, copy=False)
-
-    #     # Output list:
-    #     lstOut = [idxPrc,
-    #               aryFuncChnk]
-
-    #     queOut.put(lstOut)
-    # *************************************************************************
-
-    # *************************************************************************
-    # *** Temporal smoothing of fMRI data & pRF time course models
-
-    def funcSmthTmp(idxPrc, aryFuncChnk, varSdSmthTmp, queOut):
-        """
-        Apply temporal smoothing to the input data.
-
-        The extend of smoothing needs to be specified as an input parameter.
-        """
-        # For the filtering to perform well at the ends of the time series, we
-        # set the method to 'nearest' and place a volume with mean intensity
-        # (over time) at the beginning and at the end.
-        aryFuncChnkMean = np.mean(aryFuncChnk,
-                                  axis=1,
-                                  keepdims=True,
-                                  dtype=np.float32)
-
-        aryFuncChnk = np.concatenate((aryFuncChnkMean,
-                                      aryFuncChnk,
-                                      aryFuncChnkMean), axis=1)
-
-        # In the input data, time goes from left to right. Therefore, we apply
-        # the filter along axis=1.
-        aryFuncChnk = gaussian_filter1d(aryFuncChnk,
-                                        varSdSmthTmp,
-                                        axis=1,
-                                        order=0,
-                                        mode='nearest',
-                                        truncate=4.0)
-
-        # Remove mean-intensity volumes at the beginning and at the end:
-        aryFuncChnk = aryFuncChnk[:, 1:-1]
-
-        # Output list:
-        lstOut = [idxPrc,
-                  aryFuncChnk.astype(np.float32, copy=False)]
-
-        queOut.put(lstOut)
     # *************************************************************************
 
     # *************************************************************************
